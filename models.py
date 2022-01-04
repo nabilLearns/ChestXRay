e149ab041e03f77da86b3d8355e299a61466dabb
Nabil Mohamed <n.mohamed135@gmail.com>
2022-01-04 10:03:40 -0500

    Create baseline CNN architecture

---
+++ models.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+
+class BaseLineCNN(nn.Module):
+  def __init__(self, num_channels=1, number_of_classes=15): # we actually have 16 classes, but fit them into vector of size 15, see label conversion code
+    super(BaseLineCNN, self).__init__()
+
+    self.conv1 = nn.Sequential(
+        nn.Conv2d(num_channels, 128, 2),
+        nn.BatchNorm2d(128),
+        nn.ReLU(),
+    )
+
+    
+    self.conv2 = nn.Sequential(
+        nn.Conv2d(128, 64, 2),
+        nn.BatchNorm2d(64),
+        nn.ReLU(),
+
+    )
+
+    self.conv3 = nn.Sequential(
+        nn.Conv2d(64, 32, 2),
+        nn.BatchNorm2d(32),
+        nn.ReLU(),
+    )
+
+    self.linear = nn.Linear(32, number_of_classes)
+
+  def __call__(self, x):
+    x = self.conv1(x)
+    x = torch.nn.functional.max_pool2d(x, (2,2))
+    x = self.conv2(x)
+    x = torch.nn.functional.max_pool2d(x, (2,2))
+    x = self.conv3(x)
+    x = torch.nn.functional.max_pool2d(x, (2,2))
+    x = x.mean((2, 3))
+    x = self.linear(x)
+    #x = torch.sigmoid(x)
+    return x
