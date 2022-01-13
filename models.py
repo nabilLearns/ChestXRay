284c9fb1e4ead5c8d9173f3c63b8c85b3ada6f43
Nabil Mohamed <n.mohamed135@gmail.com>
2022-01-12 22:02:50 -0500

    Update models.py with VGG16 architecture

--- models.py
+++ models.py
@@ -37,3 +37,72 @@ class BaseLineCNN(nn.Module):
     x = self.linear(x)
     #x = torch.sigmoid(x)
     return x
+  
+class VGG16(nn.module):
+  def __init__(self, num_channels=1, number_of_classes=16):
+    super(VGG16, self).__init__()
+    
+    self.conv1 = nn.Sequential(
+      nn.Conv2d(num_channels,64,kernel_size=3),
+      nn.Conv2d(64,64,3),
+      nn.Relu(),
+      nn.MaxPool2d(kernel_size=3,stride=2),
+      nn.Relu(),
+    )
+    
+    self.conv3 = nn.Sequential(
+      nn.Conv2d(64,128,kernel_size=3),
+      nn.Conv2d(128,128,3),
+      nn.Relu(),
+      nn.MaxPool2d(kernel_size=3,stride=2),
+      nn.Relu(),
+    )
+    
+    self.conv5 = nn.Sequential(
+      nn.Conv2d(256,256,kernel_size=3),
+      nn.Conv2d(256,256,3),
+      nn.Relu(),
+      nn.MaxPool2d(kernel_size=3,stride=2),
+      nn.Relu(),
+    )
+    
+    self.conv7 = nn.Sequential(
+      nn.Conv2d(256,512,kernel_size=3),
+      nn.Conv2d(512,512,3),
+      nn.Conv2d(512,512,3),
+      nn.Relu(),
+      nn.MaxPool2d(kernel_size=3,stride=2),
+      nn.Relu(),
+    )
+    
+    self.conv10 = nn.Sequential(
+      nn.Conv2d(512,512,kernel_size=3),
+      nn.Conv2d(512,512,3),
+      nn.Conv2d(512,512,3),
+      nn.Relu(),
+      nn.MaxPool2d(kernel_size=3,stride=2),
+      nn.Relu(),
+    )
+    
+    #self.flatten = Flatten()
+    
+    self.linear_relu_stack = nn.Sequential(
+      nn.Linear(SHAPE,4096),
+      nn.Relu(),
+      nn.Linear(4096,4096),
+      nn.Relu(),
+      nn.Linear(4096,4096),
+      nn.Relu(),
+      nn.Linear(4096,16),
+      nn.Softmax(dim=1)
+    )
+  
+  def __call__(self, x):
+    x = self.conv1(x)
+    x = self.conv3(x)
+    x = self.conv5(x)
+    x = self.conv7(x)
+    x = self.conv10(x)
+    x = x.mean((2, 3))
+    x = self.linear_relu_stack(x)
+    return x
